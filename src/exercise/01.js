726960c1f1cc39fc69d9cae30b5459ddeab37d1a
coofzilla <jeric614@yahoo.com>
2022-06-06 18:57:32 +0900

    prefetch w/webpack magic comments

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,12 @@
 
 import * as React from 'react'
 
-const loadGlobe = () => import('../globe')
-const Globe = React.lazy(loadGlobe)
+
+const Globe = React.lazy(() => import(/*webpackPrefetch: true */ '../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
       style={{
@@ -24,11 +20,7 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label
-        onMouseEnter={loadGlobe}
-        onFocus={loadGlobe}
-        style={{marginBottom: '1rem'}}
-      >
+      <label style={{marginBottom: '1rem'}}>
         <input
           type="checkbox"
           checked={showGlobe}
@@ -44,8 +36,5 @@ function App() {
     </div>
   )
 }
-// 🦉 Note that if you're not on the isolated page, then you'll notice that this
-// app actually already has a React.Suspense component higher up in the tree
-// where this component is rendered, so you *could* just rely on that one.
 
 export default App
